5d78c6dab5ab7bacecb543c44ba2939a482b878b
VadimKorobec <_s_e_t__@ukr.net>
2023-03-10 13:05:17 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,53 +1,124 @@
-console.log('First log');
+// console.log('First log');
 
-setTimeout(() => {
-  console.log('Second log');
-}, 2000);
+// setTimeout(() => {
+//   console.log('Second log');
+// }, 2000);
 
-console.log('Third log');
+// console.log('Third log');
 
-const button = document.getElementById('button');
+// const button = document.getElementById('button');
 
-const onClick = () => {
-  setTimeout(() => {
-    alert('You click me))');
-  }, 2000);
-};
+// const onClick = () => {
+//   setTimeout(() => {
+//     alert('You click me))');
+//   }, 2000);
+// };
 
-button.addEventListener('click', onClick);
+// button.addEventListener('click', onClick);
 
-const green = () => {
-  console.log('Yello');
-};
+// const green = () => {
+//   console.log('Yello');
+// };
 
-const timerId = setTimeout(green, 3000);
+// const timerId = setTimeout(green, 3000);
 
-clearTimeout(timerId);
+// clearTimeout(timerId);
 
-const btnStart = document.getElementById('start');
-const btnStop = document.getElementById('stop');
-let timer = null;
+// const btnStart = document.getElementById('start');
+// const btnStop = document.getElementById('stop');
+// let timer = null;
 
-btnStart.addEventListener('click', () => {
-  timer = setInterval(() => {
-    console.log(`I love async JS ${Math.random()}`);
-  }, 1000);
-});
+// btnStart.addEventListener('click', () => {
+//   timer = setInterval(() => {
+//     console.log(`I love async JS ${Math.random()}`);
+//   }, 1000);
+// });
 
-btnStop.addEventListener('click', () => {
-  clearInterval(timer);
-  console.log(`Interval with id ${timer} has stopped`);
-});
+// btnStop.addEventListener('click', () => {
+//   clearInterval(timer);
+//   console.log(`Interval with id ${timer} has stopped`);
+// });
 
-const date = new Date();
-console.log(date);
-console.log(date.toString());
-console.log(new Date(0));
-console.log(new Date(15000));
-console.log(date.getTime());
+// const date = new Date();
+// console.log(date);
+// console.log(date.toString());
+// console.log(new Date(0));
+// console.log(new Date(15000));
+// console.log(date.getTime());
 
-const teamMeetingDate = new Date('march 16,2030');
-console.log(teamMeetingDate);
+// const teamMeetingDate = new Date('march 16,2030');
+// console.log(teamMeetingDate);
 
-const preciseTeamMeetingDate = new Date('March 16, 2030 14:25:00');
-console.log(preciseTeamMeetingDate);
+// const preciseTeamMeetingDate = new Date('March 16, 2030 14:25:00');
+// console.log(preciseTeamMeetingDate);
+
+// new Date('2030-03-16');
+// new Date('2030-03');
+// new Date('2030');
+// new Date('03/16/2030');
+// new Date('2030/03/16');
+// new Date('2030/3/16');
+// new Date('March 16, 2030');
+// new Date('March 16, 2030 14:25:00');
+// new Date('2030-03-16 14:25:00');
+// new Date('2030-03-16T14:25:00');
+// new Date('16 March 2030');
+
+// const date1 = new Date(2030, 2, 16, 14, 25, 0, 0);
+// console.log(date1);
+
+// const date2 = new Date();
+// console.log('Date: ', date2);
+
+// // Возвращает день месяца от 1 до 31
+// console.log('getDate(): ', date2.getDate());
+
+// // Возвращает день недели от 0 до 6
+// console.log('getDay(): ', date2.getDay());
+
+// // Возвращает месяц от 0 до 11
+// console.log('getMonth(): ', date2.getMonth());
+
+// // Возвращает год из 4 цифр
+// console.log('getFullYear(): ', date2.getFullYear());
+
+// // Возвращает час
+// console.log('getHours(): ', date2.getHours());
+
+// // Возвращает минуты
+// console.log('getMinutes(): ', date2.getMinutes());
+
+// // Возвращает секунды
+// console.log('getSeconds(): ', date2.getSeconds());
+
+// // Возвращает миллисекунды
+// console.log('getMilliseconds(): ', date2.getMilliseconds());
+
+// const isSuccess = true;
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve('Success! Value passed to resolve function');
+//     } else {
+//       reject('Error! Error passed to reject function');
+//     }
+//   }, 2000);
+// });
+
+// console.log('Before promise.then()');
+
+// promise.then(
+//   value => {
+//     console.log('onResolve call inside promise.then()');
+//     console.log(value);
+//   },
+//   error => {
+//     console.log('onReject call inside promise.then()');
+//     console.log(error);
+//   }
+// );
+
+// console.log('After promise.then()');
+
+// const fetchUserFromServer = (username, onSuccess, onError);
