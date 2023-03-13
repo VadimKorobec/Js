18907195036ad35086412d670337f30fed08b917
VadimKorobec <_s_e_t__@ukr.net>
2023-03-13 15:08:46 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -302,54 +302,54 @@
 
 // LxIRfOX2f4WDBIqactpC;
 
-const BASE_URL = 'https://the-one-api.dev/v2/character';
+// const BASE_URL = 'https://the-one-api.dev/v2/character';
 
-const list = document.getElementById('list');
-const load = document.getElementById('load-more');
+// const list = document.getElementById('list');
+// const load = document.getElementById('load-more');
 
-load.addEventListener('click', onLoad);
+// load.addEventListener('click', onLoad);
 
-let page = 1;
+// let page = 1;
 
-function ringsApi(page = 1) {
-  const options = {
-    headers: {
-      Authorization: 'Bearer LxIRfOX2f4WDBIqactpC',
-    },
-  };
-  return fetch(`${BASE_URL}?limit=300&page=${page}`, options).then(response => {
-    if (!response.ok) {
-      throw new Error(console.log(error));
-    }
-    return response.json();
-  });
-}
+// function ringsApi(page = 1) {
+//   const options = {
+//     headers: {
+//       Authorization: 'Bearer LxIRfOX2f4WDBIqactpC',
+//     },
+//   };
+//   return fetch(`${BASE_URL}?limit=300&page=${page}`, options).then(response => {
+//     if (!response.ok) {
+//       throw new Error(console.log(error));
+//     }
+//     return response.json();
+//   });
+// }
 
-ringsApi()
-  .then(data => {
-    list.insertAdjacentHTML('beforeend', createMarkup(data.docs));
-  })
-  .catch(error => console.log(error));
+// ringsApi()
+//   .then(data => {
+//     list.insertAdjacentHTML('beforeend', createMarkup(data.docs));
+//   })
+//   .catch(error => console.log(error));
 
-function createMarkup(arr) {
-  return arr
-    .map(
-      ({ name, race }) => `<li>
-    <h2>${name}</h2>
-    <p>${race}</p>
-  </li>`
-    )
-    .join('');
-}
+// function createMarkup(arr) {
+//   return arr
+//     .map(
+//       ({ name, race }) => `<li>
+//     <h2>${name}</h2>
+//     <p>${race}</p>
+//   </li>`
+//     )
+//     .join('');
+// }
 
-function onLoad() {
-  page += 1;
-  ringsApi(page)
-    .then(data => {
-      list.insertAdjacentHTML('beforeend', createMarkup(data.docs));
-      if (data.page === data.pages) {
-        load.setAttribute('hidden', true);
-      }
-    })
-    .catch(error => console.log(error));
-}
+// function onLoad() {
+//   page += 1;
+//   ringsApi(page)
+//     .then(data => {
+//       list.insertAdjacentHTML('beforeend', createMarkup(data.docs));
+//       if (data.page === data.pages) {
+//         load.setAttribute('hidden', true);
+//       }
+//     })
+//     .catch(error => console.log(error));
+// }
